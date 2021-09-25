6d5c671d763a9c95b0d3f1fe1d4fa95769cd543e
Joe <78252830+benedictvs@users.noreply.github.com>
2021-09-25 00:40:09 -0500

    Add files via upload

---
+++ romansolverfc.py
@@ -0,0 +1,97 @@
+from questionary import Separator, prompt, print
+from pprint import pprint
+import os
+
+'''
+Here is a good example of how simple it can be to add modules. This is all
+based off of the Euclid solver code. This contains every single line of code
+that pertains to the Roman numeral solver. The only line I had to add to the
+main file, was "from romansolverfc import *". Unfortunately, the solving
+functions are a little sloppy, and could use some further revision. 
+'''
+
+
+
+#————————————————————————————————————————————————
+#MODULE PROMPTS
+#————————————————————————————————————————————————
+
+def prompt_roman_to_dec(**kwargs):
+    questions = [
+        {
+            "qmark": "ROMAN",
+            "type": "text",
+            "name": "user_input",
+            "message": "Please enter the Roman numeral expression > ",
+            "validate": lambda val: val.isalpha() ,
+        },
+    ]
+    return prompt(questions)
+
+
+#————————————————————————————————————————————————
+#SOLVER FUNCTIONS
+#————————————————————————————————————————————————
+
+nums = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100,'D':500, 'M':1000}
+def roman(expression: str) -> 'Returns integer':
+    romalist = list(expression)
+    if len(romalist) == 1:
+        return nums[expression]
+    sums = []
+    for i in range(0, len(romalist)-1):
+        if nums[romalist[i]] < nums[romalist[i+1]]:
+            sums.append(nums[romalist[i]]*-1)
+        else:
+            sums.append(nums[romalist[i]])
+    sums.append(nums[romalist[-1]])
+    return sum(sums)
+
+def romanwork(expression: str) -> 'Returns string containing work':
+    romalist = list(expression)
+    unique_romalist = set(romalist)
+    unique_romalist = list(unique_romalist)
+    recallstr = 'Recall that: '
+    for i in range(0,len(unique_romalist)-1):
+        recallstr += unique_romalist[i] + ' = ' + str(nums[unique_romalist[i]]) + ', '
+    recallstr += 'and ' + unique_romalist[-1] + ' = ' + str(nums[unique_romalist[-1]])
+    if len(romalist) == 1:
+        return nums[expression]
+    sums = []
+    formula = ''
+    for i in range(0, len(romalist)-1):
+        if nums[romalist[i]] < nums[romalist[i+1]]:
+            sums.append(nums[romalist[i]]*-1)
+            formula += ' ' + romalist[i] + '(' + str(nums[romalist[i]]*-1) + ') +'
+        else:
+            sums.append(nums[romalist[i]])
+            formula += ' ' + romalist[i] + '(' + str(nums[romalist[i]]) + ') +'
+    formula += ' ' + romalist[-1] + '(' + str(nums[romalist[-1]]) + ')'
+    sums.append(nums[romalist[-1]])
+    val = sum(sums)
+    return recallstr + '\n' + formula + '\n' + '= {}'.format(val)
+
+
+#————————————————————————————————————————————————
+#OBJECT DEFINITION OF ANSWER
+#————————————————————————————————————————————————
+
+class romansolvermodule:
+    def __init__(self, expression: str):
+        self.expression = expression
+        self.ans = roman(expression)
+        self.work = romanwork(expression)
+
+#————————————————————————————————————————————————
+#MAIN FUNCTION
+#————————————————————————————————————————————————
+
+def romansolver():
+    #Define the first euclid argument as global so that we can compare the second input to it
+    global x_arg
+    exp = prompt_roman_to_dec()['user_input']
+
+    solution = romansolvermodule(exp)
+    print('\nYour answer: {}'.format(solution.ans), style="bold italic fg:yellow")
+    print('Work:\n{}'.format(solution.work), style="bold italic fg:yellow")
+    input('\nPlease hit enter when you are finished.')
