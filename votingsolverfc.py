3309cb23f97c06d2a93f72121f5532d6bcbf78b0
gwild37 <gabewild37@gmail.com>
2021-10-25 19:03:04 -0400

    updated UI to be a bit better

--- votingsolverfc.py
+++ votingsolverfc.py
@@ -1,5 +1,6 @@
 from questionary import Separator, prompt, print
 from pprint import pprint
+from itertools import permutations
 import os
 
 '''
@@ -22,8 +23,8 @@ def prompt_input_num_candidates(**kwargs):
             "qmark": "VOTING",
             "type": "text",
             "name": "num_candidates",
-            "message": "Please enter the number of candidates (1-100) > ",
-            "validate": lambda val: val.isdigit() and int(val) >= 1 and int(val) <= 100,
+            "message": "Please enter the number of candidates (1-10) > ",
+            "validate": lambda val: val.isdigit() and int(val) >= 1 and int(val) <= 10,
         },
     ]
     return prompt(questions)
@@ -42,27 +43,26 @@ def prompt_input_candidates(num_candidates, candidates, **kwargs):
     return prompt(questions)
 
 
-def prompt_input_num_votes(**kwargs):
-    questions = [
-        {
-            "qmark": "VOTING",
-            "type": "text",
-            "name": "num_votes",
-            "message": "Please enter the number of votes > (1-1000)",
-            "validate": lambda val: val.isdigit() and int(val) >= 1 and int(val) <= 1000,
-        },
-    ]
-    return prompt(questions)
-
-
-def prompt_input_votes(num_votes, num_candidates, candidate_options, **kwargs):
+def prompt_input_permutation(vote_permutations, **kwargs):
     questions = [
         {
             "qmark": "VOTING",
             "type": "select",
-            "name": "vote" + str(num_votes) + "-" + str(num_candidates),
-            "message": "Vote #" + str(num_votes + 1) + ": Rank the candidates from best to worst > ",
-            "choices": candidate_options
+            "name": "permutation",
+            "message": "Which permutations have votes >",
+            "choices": vote_permutations,
+        },
+    ]
+    return prompt(questions)
+
+def prompt_input_num_votes(num_permutation, **kwargs):
+    questions = [
+        {
+            "qmark": "VOTING",
+            "type": "text",
+            "name": "num_votes" + str(num_permutation),
+            "message": "Please enter the number of votes for this permutation > ",
+            "validate": lambda val: val.isdigit() and int(val) >= 0,
         },
     ]
     return prompt(questions)
@@ -79,10 +79,33 @@ class voting:
         self.count = dict()
         for c in self.candidates:
             self.count[c] = 0
+
+    def str_ans(self, count, winners):
+        ret = ""
+        for c in self.candidates:
+            ret += "\t" + str(c) + ": " + str(count[c]) + "\n"
+        if len(winners) == 1:
+            ret += "\tWinner: " + winners[0]
+        else:
+            ret += "\tWinners: " + ", ".join(winners)
+        return ret
     
     def ans(self):
-        return "Borda: " + str(self.borda()) + "\nPlurality: " + str(self.plurality())
-    
+        ret = ""
+        # ————————————————————————————————————————————————
+        # PLURALITY
+        # ————————————————————————————————————————————————
+        count_plurality, winners_plurality = self.plurality()
+        ret += "Plurality: \n"
+        ret += self.str_ans(count_plurality, winners_plurality)
+        # ————————————————————————————————————————————————
+        # BORDA
+        # ————————————————————————————————————————————————
+        count_borda, winners_borda = self.borda()
+        ret += "\nBorda: \n"
+        ret += self.str_ans(count_borda, winners_borda)
+        return ret
+
     def work(self):
         pass
             
@@ -90,7 +113,9 @@ class voting:
         count = self.count.copy()
         for v in self.votes:
             count[v[0]] += 1
-        return count
+        max_val =  max(count.values())
+        winners = [key for key, val in count.items() if val == max_val]
+        return count, winners
 
     def borda(self):
         count = self.count.copy()
@@ -98,7 +123,9 @@ class voting:
         for v in self.votes: 
             for i in range(n):
                 count[v[i]] += n-i
-        return count
+        max_val =  max(count.values())
+        winners = [key for key, val in count.items() if val == max_val]
+        return count, winners
 
 
 # ————————————————————————————————————————————————
@@ -124,19 +151,28 @@ def votingsolver():
     for i in range(num_candidates):
         candidate = str(prompt_input_candidates(i, candidates)['candidate' + str(i)])
         candidates.append(candidate)
-    num_votes = int(prompt_input_num_votes()['num_votes'])
+
+    vote_permutations = list(permutations(candidates))
+    str_vote_permutations = vote_permutations.copy()
+    for i in range(len(str_vote_permutations)):
+        str_vote_permutations[i] = " > ".join(str_vote_permutations[i])
+
+    str_vote_permutations.insert(0, "Done")
+
+    permutation_index = str_vote_permutations.index(prompt_input_permutation(str_vote_permutations)['permutation'])
+    print(str(permutation_index))
+
     votes = []
-    for i in range(num_votes):
-        _candidates = candidates.copy()
-        vote = []
-        for j in range(len(candidates)):
-            choice = str(prompt_input_votes(i, j, _candidates)
-                         ['vote' + str(i) + "-" + str(j)])
-            _candidates.remove(choice)
-            vote.append(str(choice))
-        votes.append(vote)
+    while permutation_index != 0:
+        num_votes = int(prompt_input_num_votes(permutation_index)["num_votes" + str(permutation_index)])
+        for _ in range(num_votes):
+            votes.append(vote_permutations[permutation_index-1])
+        str_vote_permutations.pop(permutation_index)
+        vote_permutations.pop(permutation_index-1)
+        permutation_index = str_vote_permutations.index(prompt_input_permutation(str_vote_permutations)['permutation'])
+        print(str(permutation_index))
     
     solution = votingsolvermodule(candidates, votes)
-    print('\nYour answer: {}'.format(solution.ans), style="bold italic fg:yellow")
+    print('\nYour answers:\n{}'.format(solution.ans), style="bold italic fg:yellow")
     # print('Work:\n{}'.format(solution.work), style="bold italic fg:yellow")
     input('\nPlease hit enter when you are finished.')
