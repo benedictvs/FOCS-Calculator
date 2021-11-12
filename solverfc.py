40144fd94ecdffb102949d5590556366dfb6f18e
gwild37 <gabewild37@gmail.com>
2021-11-12 17:08:25 -0500

    Added test class to verify and fix issues with the python abstract classes

--- solverfc.py
+++ solverfc.py
@@ -1,58 +1,9 @@
+import sys
+import os
+import math as m
+from pprint import pprint
 from abc import ABC, abstractmethod
 from questionary import Separator, prompt, print
-import main
-
-# ————————————————————————————————————————————————
-# SOLVER INTERFACE
-# ————————————————————————————————————————————————
-
-
-class solver(ABC):
-    def __init__(self, name: str, model: object) -> None:
-        self.name = name
-        self.model = model
-        self.inputs: tuple = None
-
-    def __str__(self) -> str:
-        return self.name
-
-    def __call__(self) -> tuple:
-        """
-        Call the solver like a function to run its intended exection order
-        """
-        self.prompt_inputs()
-        ans, work = self.model(self.inputs)
-        return ans, work
-
-    @abstractmethod
-    def prompt_inputs(self) -> None:
-        """
-        This method should prompt the user for all the necessary inputs. Some 
-        useful helper functions for common input prompts are provided below, 
-        but developers can write their own input prompts. Once inputs are 
-        collected, write them to self.inputs
-        """
-        pass
-
-    def prompt_integer(qmark: str, message_text: str, lower_bound: int = None, upper_bound: int = None):
-        """
-        Enter the message text you want to be displayed to the user, as well as 
-        the qmark, which is what you want the interface to identify itself as. 
-        Additionally, enter the upper/lower bounds (using >= and <=) of the 
-        integer (if there are any).
-        """
-        questions = [
-            {
-                "qmark": qmark,
-                "type": "text",
-                "name": "integer",
-                "message": message_text,
-                "validate": lambda val: val.isdigit() and\
-                     (True if lower_bound is None else int(val) >= lower_bound) and\
-                          (True if upper_bound is None else int(val) <= upper_bound),
-            },
-        ]
-        return prompt(questions)
 
 # ————————————————————————————————————————————————
 # SOLVER MODEL INTERFACE
@@ -84,3 +35,67 @@ class solver_model(ABC):
         if necessary. Write answer to self.ans and work to self.work
         """
         pass
+
+# ————————————————————————————————————————————————
+# SOLVER INTERFACE
+# ————————————————————————————————————————————————
+
+
+class solver(ABC):
+    def __init__(self, name: str, model: solver_model) -> None:
+        self.name = name
+        self.model = model
+        self.inputs: tuple = None
+        self.ans : str = None
+        self.work : str = None
+
+    def __str__(self) -> str:
+        return self.name
+
+    def __call__(self) -> None:
+        """
+        Call the solver like a function to run its intended exection order
+        """
+        self.prompt_inputs()
+        if self.inputs is None:
+            raise ValueError("self.inputs not set in prompt_inputs")
+        _model = self.model(self.inputs)
+        self.ans, self.work = _model()
+        self.prompt_outputs()
+
+    def prompt_outputs(self) -> None:
+        print('\nYour answers:\n{}'.format(self.ans),
+          style="bold italic fg:yellow")
+        print('Work:\n{}'.format(self.work), style="bold italic fg:yellow")
+        input('\nPlease hit enter when you are finished.')
+        
+    @abstractmethod
+    def prompt_inputs(self) -> None:
+        """
+        This method should prompt the user for all the necessary inputs. Some 
+        useful helper functions for common input prompts are provided below, 
+        but developers can write their own input prompts. Once inputs are 
+        collected, write them to self.inputs
+        """
+        pass
+
+    def prompt_integer(self, message_text: str, lower_bound: int = None, upper_bound: int = None) -> int:
+        """
+        Enter the message text you want to be displayed to the user, as well as 
+        the qmark, which is what you want the interface to identify itself as. 
+        Additionally, enter the upper/lower bounds (using >= and <=) of the 
+        integer (if there are any).
+        """
+        print("HERE")
+        questions = [
+            {
+                "qmark": str(self),
+                "type": "text",
+                "name": "integer",
+                "message": message_text,
+                "validate": lambda val: val.isdigit() and\
+                     (True if lower_bound is None else int(val) >= lower_bound) and\
+                          (True if upper_bound is None else int(val) <= upper_bound),
+            },
+        ]
+        return int(prompt(questions)['integer'])
