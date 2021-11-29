4c8ad324deec76927f68618d838bcb1c410405fa
gwild37 <gabewild37@gmail.com>
2021-11-29 15:21:22 -0500

    lambda calclus fixed, attribution properly added, and logic gate documentation added

--- modules/lambdacalculus.py
+++ modules/lambdacalculus.py
@@ -2,25 +2,85 @@ from pylambdac import parse
 
 from abstractclasses import solver, solver_model
 
+"""
+lambdacalculus takes a lambda calculus expression and reduces it to weak head normal form
+"""
+
+# ————————————————————————————————————————————————
+# LAMBDA CALCULUS MODEL CLASS
+# ————————————————————————————————————————————————
+
 
 class lambda_calculus_solver(solver):
-    def prompt_inputs(self) -> None:
-        expr = self.prompt_string("Enter a lambda calculus expression")
+    def convert_lambda(self, expr) -> str:
+        expr = str(expr)
+        if "lambda" in expr:
+            expr = expr.replace("lambda", "λ")
+        if "Lambda" in expr:
+            expr = expr.replace("Lambda", "λ")
+        return expr
 
-        self.inputs['expr'] = expr
+    def prompt_inputs(self) -> None:
+        def validate(val, *args) -> bool:
+            """
+            Check for invalid lambda calculus expression
+            """
+            try:
+                val = self.convert_lambda(val)
+                parse_lambda_expr(val)
+            except:
+                return False
+            return True
+
+        expr = self.prompt_string(
+            "Enter a valid lambda calculus expression > ", validate
+        )
+        expr = self.convert_lambda(expr)
+        print(expr)
+        self.inputs["expr"] = expr
+
+
+# ————————————————————————————————————————————————
+# LAMBDA CALCULUS MODEL CLASS
+# ————————————————————————————————————————————————
 
 
 class lambda_calculus_model(solver_model):
     def solve(self) -> None:
-        expr = self.inputs['expr']
-        work = self.parse_lambda_expr(expr)
+        expr = self.inputs["expr"]
+        work = parse_lambda_expr(expr)
         self.ans = str(work[-1])
-        self.work = str(work)
+        it = 1
+        if len(work) == 1:
+            self.work = (
+                "lambda calculus expression already in weak head normal form"
+            )
+        else:
+            self.work = "Steps to reduce:\n" + "{:d}. ".format(it) + expr + "\n"
+            for ex in work:
+                it += 1
+                self.work += "{:d}. ".format(it) + str(ex) + "\n"
 
-    def parse_lambda_expr(self, expr):
-        new = []
-        state = parse.parse_expr(expr)
-        while state is not None:
-            new.append(str(state))
-            state = state.reduce_once({})
-        return new
+
+# Copyright 2018 Paul Crowley
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+def parse_lambda_expr(expr):
+    new = []
+    state = parse.parse_expr(expr)
+    while state is not None:
+        new.append(str(state))
+        state = state.reduce_once({})
+    return new
